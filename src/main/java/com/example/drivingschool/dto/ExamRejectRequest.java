c7dd1f2c79d10f5e82ec41285fac70e54c40e5be
LeviPan <13201006226@163.com>
2026-06-10 20:41:28 +0800

    6.10 Fix exam c1ompilation issue

---
+++ src/main/java/com/example/drivingschool/dto/ExamRejectRequest.java
@@ -0,0 +1,13 @@
+package com.example.drivingschool.dto;
+
+public class ExamRejectRequest {
+    private String reason;
+
+    public String getReason() {
+        return reason;
+    }
+
+    public void setReason(String reason) {
+        this.reason = reason;
+    }
+}
