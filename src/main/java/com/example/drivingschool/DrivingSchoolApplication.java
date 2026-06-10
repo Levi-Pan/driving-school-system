15168ddc750e63fa4531a08b68b237dff972531d
LeviPan <13201006226@163.com>
2026-06-10 22:52:43 +0800

    6.10.1.1

--- src/main/java/com/example/drivingschool/DrivingSchoolApplication.java
+++ src/main/java/com/example/drivingschool/DrivingSchoolApplication.java
@@ -3,6 +3,7 @@ package com.example.drivingschool;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class DrivingSchoolApplication {
     public static void main(String[] args) {
